90540419632c259f401c4a85a9b438534e11f43d
RXiau6 <qq5565227@gmail.com>
2021-12-02 17:07:38 +0800

    salting func

--- Backend/src/crud.py
+++ Backend/src/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-
+import random,string,hashlib
 from . import models, schemas
 
 def get_user(db: Session, user_id: int):
@@ -13,9 +13,24 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
+    salt = salt_gen()
+    
+    fake_hashed_password = user.password + salt
     db_user = models.User(email=user.email, hashed_passwd=fake_hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def salt_gen():
+    letter = []
+    for i in range(1,16):
+        chars = random.choice(string.ascii_letters+string.digits)
+        letter.append(chars)
+    
+
+    salt = ''.join(str(e) for e in letter)
+    return salt
+
+def hashing():
+    
