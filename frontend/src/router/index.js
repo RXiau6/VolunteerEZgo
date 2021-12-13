1f4d38da0c118bf2cd45e7c36cdf0c5c5f9b959e
C109118205 <C109118205@nkust.edu.tw>
2021-12-14 00:03:54 +0800

    home page updated

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  
+  { 
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+  { 
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
   {
+    
     path: '/Login',
     name: 'Login',
     component: () => import('../views/Login.vue')
