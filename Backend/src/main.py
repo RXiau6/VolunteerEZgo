491a68dfffe8105ec2e5377b5013f39a41e5e718
RXiau6 <qq5565227@gmail.com>
2021-12-17 00:05:35 +0800

    laptop change

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -12,12 +12,16 @@ from sqlalchemy.sql.expression import column
 
 #from . import crud, models, schemas
 from . import crud, models, schemas
+from . import cookie as cookies
+from uuid import UUID
 from .database import SessionLocal, engine
 from .config import SQLALCHEMY_DATABASE_URL
 
 #cookie - front
 
 from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+
+from . import config
 
 cookie_params = CookieParameters()
 
@@ -26,60 +30,18 @@ cookie = SessionCookie(
     cookie_name="cookie",
     identifier="general_verifier",
     auto_error=True,
-    secret_key="DONOTUSE",
+    secret_key=config.key,
     cookie_params=cookie_params,
 )
 
-
-#session -backend 
-from uuid import UUID
-from fastapi_sessions.backends.implementations import InMemoryBackend
-
-backend = InMemoryBackend[UUID, schemas.SessionData]()
-
-#session BasicVerifier
-from fastapi_sessions.session_verifier import SessionVerifier
-from fastapi import HTTPException
-
-class BasicVerifier(SessionVerifier[UUID, schemas.SessionData]):
-    def __init__(
-        self,
-        *,
-        identifier: str,
-        auto_error: bool,
-        backend: InMemoryBackend[UUID, schemas.SessionData],
-        auth_http_exception: HTTPException,
-    ):
-        self._identifier = identifier
-        self._auto_error = auto_error
-        self._backend = backend
-        self._auth_http_exception = auth_http_exception
-
-    @property
-    def identifier(self):
-        return self._identifier
-
-    @property
-    def backend(self):
-        return self._backend
-
-    @property
-    def auto_error(self):
-        return self._auto_error
-
-    @property
-    def auth_http_exception(self):
-        return self._auth_http_exception
-
-    def verify_session(self, model: schemas.SessionData) -> bool:
-        """If the session exists, it is valid"""
-        return True
+# from .cookie import BasicVerifier,backend
 
 
-verifier = BasicVerifier(
+
+verifier = cookies.BasicVerifier(
     identifier="general_verifier",
     auto_error=True,
-    backend=backend,
+    backend=cookies.backend,
     auth_http_exception=HTTPException(status_code=403, detail="invalid session"),
 )
 
@@ -149,7 +111,7 @@ async def create_session(name: str, response: Response):
     session = uuid4()
     data = schemas.SessionData(username=name,expired=datetime.datetime.now() + datetime.timedelta(seconds=30))
 
-    await backend.create(session, data)
+    await cookies.backend.create(session, data)
     cookie.attach_to_response(response, session)
 
     return f"created session for {name}"
@@ -164,6 +126,6 @@ async def whoami(session_data: schemas.SessionData = Depends(verifier)):
 
 @app.post("/delete_session")
 async def del_session(response: Response, session_id: UUID = Depends(cookie)):
-    await backend.delete(session_id)
+    await cookies.backend.delete(session_id)
     cookie.delete_from_response(response)
     return "deleted session"
